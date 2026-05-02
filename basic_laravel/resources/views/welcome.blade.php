a461ed0ba4f7cd4f870897d5b5dc7d097fb00000
anhar0401 <mfadhilanhar_2428240158@mhs.mdp.ac.id>
2026-05-02 20:36:59 +0700

    Add : [Fakultas view]

--- basic_laravel/resources/views/welcome.blade.php
+++ basic_laravel/resources/views/welcome.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    @vite([])
-</head>
-<body>
-    {{ $slot }}
-</body>
-</html>
+<x-layout>
+    <h1>Welcome</h1>
+    <a href="/fakultas">List Fakultas</a>
+    <a href="/edit">Edit Fakultas</a>
+    <a href="/detail">Detail Fakultas</a>
+</x-layout>
